9385f1d21142a2e662c8c267274835c84919ab1b
rm <rolandas.markev@gmail.com>
2026-02-08 23:38:55 +0100

    chapter 5 WIP

--- main.py
+++ main.py
@@ -22,6 +22,10 @@ class Softmax:
         negative_exponents = np.exp(input - np.max(input, axis=1, keepdims=True))
         self.output = negative_exponents / np.sum(negative_exponents, axis=1, keepdims=True)
 
+class CCE_loss:
+    def forward(self, input, labels):
+        self.output = np.mean(-np.log(input[range(len(input)), labels]))
+
 #define the dataset
 x, y = spiral_data(samples=100, classes=3)
 
@@ -30,6 +34,7 @@ l1 = Layer(2,3)
 l1_relu = Activation()
 l2 = Layer(3,3)
 l2_softmax = Softmax()
+loss = CCE_loss()
 
 #forward pass
 l1.forward(x)
@@ -38,4 +43,9 @@ l1_relu.forward(l1.output)
 l2.forward(l1_relu.output)
 l2_softmax.forward(l2.output)
 
-print(l2_softmax.output[:5])
+#loss
+loss.forward(l2_softmax.output, y)
+
+
+#outputs
+print(loss.output)
