6b7b2e681b035b266ab96784b366bc3f386fc8eb
rm <rolandas.markev@gmail.com>
2026-02-08 16:03:15 +0000

    renamed files

---
+++ main.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib as plt
+import nnfs
+from nnfs.datasets import spiral_data
+
+nnfs.init()
+
+class Layer:
+    def __init__(self, nr_inputs, nr_neurons):
+        self.weights = 0.01 * np.random.randn(nr_inputs, nr_neurons)
+        self.bias = np.zeros(nr_neurons)
+
+    def forward(self, inputs):
+        self.output = np.dot(self.weights, inputs) + self.bias
+
+class Activation:
+    def forward(self, input):
+        self.output = np.max(0, input, axis=1, keepdims=True)
+
+class Softmax:
+    def forward(self, input):
+        negative_exponents = np.max(input) - input
+        self.output = input
+
+        pass
+
+#define the dataset
+x, y = spiral_data(samples=100, classes=3)
+
+#network architecture
+l1 = Layer(2,3)
+l1_relu = Activation()
+l2 = Layer(3,3)
+l2_softmax = Softmax()
+
+#forward pass
+l1.forward(x)
+l1_relu.forward(l1.output)
+
+l2.forward(l1_relu.output)
+l2_softmax.forward(l2.output)
+
+print(l2_softmax.output[:5])
--- nnfs.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import matplotlib as plt
-from nnfs.datasets import spiral_data
-
-import nnfs
-
